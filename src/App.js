d66c01a0e00fe113cbb7f2747a7065b1106f6857
Jimmy <dabrowski.mac.1@gmail.com>
2022-05-30 15:45:12 +0200

    change componets to be reusable

--- src/App.js
+++ src/App.js
@@ -8,7 +8,9 @@ import React, { Component } from "react";
 import uniqid from "uniqid";
 import GeneralInfo from "./components/general-info";
 import Education from "./components/education";
-import PrintSchools from "./components/show-schools";
+import Experience from "./components/experience";
+import ShowSchools from "./components/show-schools";
+import ShowJobs from "./components/show-jobs";
 
 class App extends Component
 {
@@ -38,14 +40,15 @@ class App extends Component
       experience: {
         comapnyName: "",
         position: "",
-        tasks: [],
-        date: "",
+        description: "",
+        jobStart: "",
+        jobEnd: "",
+        id: uniqid(),
       },
     };
-
-    this.handleChange.bind(this);
-    this.onSubmitSchool.bind(this);
-    this.onDeleteSchool.bind(this);
+    // this.handleChange.bind(this);
+    // this.onSubmitSchool.bind(this);
+    // this.onDeleteSchool.bind(this);
   }
 
   handleChange = (e) =>
@@ -80,6 +83,27 @@ class App extends Component
     }));
   };
 
+  onSubmitExperience = (e) =>
+  {
+    e.preventDefault();
+    this.setState((prevState) => ({
+      ...prevState,
+      personalData: {
+        ...prevState.personalData,
+        jobs: this.state.personalData.jobs.concat(this.state.experience),
+      },
+      experience: {
+        comapnyName: "",
+        position: "",
+        description: "",
+        jobStart: "",
+        jobEnd: "",
+        id: uniqid(),
+      },
+    }));
+    console.log(this.state.personalData.jobs.length);
+  };
+
   onDeleteSchool = (e) =>
   {
     const { personalData } = this.state;
@@ -88,6 +112,18 @@ class App extends Component
       personalData: {
         ...prevState.personalData,
         schools: personalData.schools.filter((school) => school.id !== e.target.parentElement.dataset.key),
+      },
+    }));
+  };
+
+  onDelete = (e, key) =>
+  {
+    const { personalData } = this.state;
+    this.setState((prevState) => ({
+      ...prevState,
+      personalData: {
+        ...prevState.personalData,
+        [key]: personalData[key].filter((el) => el.id !== e.target.parentElement.dataset.key),
       },
     }));
   };
@@ -113,19 +149,25 @@ class App extends Component
         </p>
         <h2>Education</h2>
         <div className="education">
-          <PrintSchools
-            personalData={personalData}
-            onDeleteSchool={this.onDeleteSchool}
-          />
           <Education
             handleChange={this.handleChange}
             onSubmitSchool={this.onSubmitSchool}
             education={education}
           />
+          <ShowSchools
+            personalData={personalData}
+            onDelete={this.onDelete}
+          />
         </div>
+        <h2>Experience</h2>
+        <Experience
+          experience={experience}
+          handleChange={this.handleChange}
+          onSubmitExperience={this.onSubmitExperience}
+        />
+        <ShowJobs personalData={personalData} onDelete={this.onDelete} />
       </div>
     );
   }
 }
-
 export default App;
